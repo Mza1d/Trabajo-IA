570b33063d683db554960effe7a788f9f1e3591e
Mario A. Zaid <mad.tkm@gmail.com>
2022-12-11 21:22:40 -0400

    ia

---
+++ Tema 1/Ejercicio III/AgenteAspiradora.py
@@ -0,0 +1,200 @@
+#Univ.: Anarata Diaz Mario
+
+#IMPORTAMOS LIBRERIAS
+import random as rd # para Aleatorio
+import matplotlib.pyplot as plt #Para graficar
+
+def main():
+    agente = input("\tSELECCIONE\nAgente reactivo simple '0' \nAgente basado en objetivos '1'\n")
+    matriz = definirAmbiente() #crea ambiente
+    # muestra el entorno en la consola
+    for i in matriz:
+        print(i)
+    posicion = [1,1] #posicion inicial
+    puntos = 0 #iniciar puntos variables
+    accion = 0 #iniciar acción variable
+    if( agente == '1'): #Agente basado en objetivo
+        estado = estadoActual(matriz,posicion) #estado en la posicion actual
+        percepcion = [posicion,estado] #percepcion que contiene posicion + estado
+        mostrar(matriz,percepcion[0]) #muestra el entorno
+        while(accion != 6): #hasta que se alcance la meta
+            accion = agenteObjetivo(matriz,percepcion,checkObj(matriz)) #guarda la acción que debe realizar el Aspirador o limpiador
+            if(accion != 6): # si no fuera por que la aspiradora se detuviera
+                puntos +=1 #contar puntos
+                percepcion = ejecutarAccion(matriz,percepcion,accion) #ejecución
+                mostrar(matriz,percepcion[0]) #mostrar mapa
+        print('Puntaje total: '+str(puntos)) #imprimir los puntos
+
+        #Agente reactivo simple
+    elif(agente == '0'):
+        posicion= [rd.choice([1,2,3,4]),rd.choice([1,2,3,4])] #posicion inicial aleatoria de la aspiradora 
+        estado = estadoActual(matriz,posicion) #estado
+        percepcion = [posicion,estado] #percepcion que contiene posicion + estado
+        mostrar(matriz,percepcion[0]) #mostrar
+        mapa = funcionMapa() # función de mapa
+        while True: # Circulo
+            percepcion = ejecutarAccion(matriz,percepcion, agenteReativoSimples(matriz,mapa,percepcion))  #percepcion del robot después de ejecutar la acción      
+            mostrar(matriz,percepcion[0]) #mostrar
+    
+    
+def mostrar(I, posicion):    
+    plt.imshow(I)
+    plt.nipy_spectral() 
+    plt.plot(posicion[0],posicion[1], marker='H', color='r', ls='')
+    plt.pause(0.25)    
+    #plt.clf()
+    
+def definirAmbiente(): # Define el entorno, donde 1 = pared, 0 = limpio, 2 = sucio (donde las posiciones de suciedad son aleatorias)
+    matriz = [[1,1,1,1,1,1],
+              [1,rd.choice([0,2]),rd.choice([0,2]),rd.choice([0,2]),rd.choice([0,2]),1],
+              [1,rd.choice([0,2]),rd.choice([0,2]),rd.choice([0,2]),rd.choice([0,2]),1],
+              [1,rd.choice([0,2]),rd.choice([0,2]),rd.choice([0,2]),rd.choice([0,2]),1],
+              [1,rd.choice([0,2]),rd.choice([0,2]),rd.choice([0,2]),rd.choice([0,2]),1],
+              [1,1,1,1,1,1]]
+    return matriz
+
+def funcionMapa(): # funcion Mapear
+    funcionMapa = [[1,1,4],
+                    [2,1,4],
+                    [3,1,4],
+                    [4,1,2],
+                    [4,2,3],
+                    [3,2,3],
+                    [2,2,2],
+                    [2,3,4],
+                    [3,3,4],
+                    [4,3,2],
+                    [4,4,3],
+                    [3,4,3],
+                    [2,4,3],
+                    [1,4,1],
+                    [1,3,1],
+                    [1,2,1]]
+    return funcionMapa
+
+def estadoActual(matriz,posicion): # devuelve el estado actual en la posicion dada
+    return matriz[posicion[1]][posicion[0]]
+
+def agenteObjetivo(matriz,percepcion,objObtenido):
+
+    if(objObtenido == 0): # si se cumplió el objetivo
+        return 6 # para detener
+    
+    posicion = percepcion[0] #posicion atual
+    estado = estadoActual(matriz,posicion) #estado actual
+    posicionObj = encontrarSuciedadMenorDistancia(matriz,percepcion[0]) #posicion Objetivo
+
+    if(estado == 2): #si esta sucio
+        print('Estado: '+str(estado)+' accion: aspirar')
+        return 'aspirar'    
+    if(posicion[1] < posicionObj[0]): #Bajar
+        print('Estado: '+str(estado)+' accion: abajo')
+        return 'abajo'
+    elif(posicion[1] > posicionObj[0]): #Subir
+        print('Estado: '+str(estado)+' accion: arriba')
+        return 'arriba'
+    if(posicion[0] < posicionObj[1]): #Derecha
+        print('Estado: '+str(estado)+' accion: derecha')
+        return 'derecha'
+    elif(posicion[0]>posicionObj[1]): #Izquierda
+        print('Estado: '+str(estado)+' accion: izquierda')
+        return 'izquierda'
+    
+def checkObj(matriz):
+    for s in matriz:
+        for n in s:
+            if(n == 2):
+                return 1
+    return 0
+
+#busca la suciedad más cercana al robot
+def encontrarSuciedadMenorDistancia(matriz,posicion):
+    if(estadoActual(matriz,posicion)==2): #si está sucio, vuelve a la posicion actual
+        return posicion
+    menorDistancia = 99999 #inicializar la variable de distancia más corta
+    posicionSuciedad = posicion #inicializar la posicion de suciedad
+    numLinea = 0 #contador de linea
+    for linea in matriz:
+        numCasa = 0 #contador de la casa
+        for casa in linea:
+            #si la casa esta sucia
+            if(casa == 2):
+                suciedad = [numLinea,numCasa] #posicion de suciedad
+
+                distancia = calcularDistancia(posicion,suciedad) #distancia a la suciedad
+                if(distancia < menorDistancia): #compara para encontrar la distancia más pequeña, si es más pequeña, guarda la posicion
+                   menorDistancia = distancia 
+                   posicionSuciedad = suciedad
+            numCasa +=1                   
+        numLinea +=1
+    return posicionSuciedad
+
+def calcularDistancia(posicionActual,posicionSuciedad): #devuelve la distancia desde la posicion actual hasta la posicion de suciedad
+    cont = 0 # contador de distancia
+    x = posicionActual[0] #posicion actual
+    y = posicionActual[1]
+    xObj = posicionSuciedad[1] #posicion objetivo
+    yObj = posicionSuciedad[0]
+    cont += distanciaRecta(y, yObj)
+    cont += distanciaRecta(x, xObj)
+    return cont
+
+def distanciaRecta(pos, obj): # distancia entre dos puntos de fila o columna
+    i = 0
+    while pos != obj: 
+        if pos > obj:
+            pos -= 1
+            i+=1
+        else:
+            pos +=1
+            i+=1
+    return i
+    
+
+#mueve el robot en función de la acción informada               
+def ejecutarAccion(matriz,percepcion,accion):
+    posicion = percepcion[0] # tomar la posicion del robot
+    estado = percepcion[1]  # obtener el estado actual
+    
+    # si para verificar la acción, cambie la posicion del robot y verifique el estado
+    if accion =='arriba':
+        posicion = [posicion[0],posicion[1]-1]
+        estado = estadoActual(matriz,posicion)
+    elif accion =='abajo':
+        posicion = [posicion[0],posicion[1]+1]
+        estado = estadoActual(matriz,posicion)
+    elif accion =='izquierda':
+        posicion = [posicion[0]-1,posicion[1]]
+        estado = estadoActual(matriz,posicion)
+    elif accion =='derecha':
+        posicion = [posicion[0]+1,posicion[1]]
+        estado = estadoActual(matriz,posicion)
+    elif accion =='aspirar':
+        matriz[posicion[1]][posicion[0]] = 0
+        estado = estadoActual(matriz,posicion)
+    
+    percepcion = [posicion,estado] # guardar la información en la percepcion
+    return percepcion # devuelve la nueva percepcion con la nueva posicion y estado del robot
+
+def agenteReativoSimples(matriz,mapa,percepcion): # devuelve la acción que debe realizar el robot
+    posicion = percepcion[0] # robot de toma de posicion
+    estado = estadoActual(matriz,posicion) # obtener estado robot
+    
+    if(estado == 2): #verificar estado
+        return 'aspirar'
+    else:
+        for a in mapa: # busca la acción que debe realizar el robot
+            if(a[0] == posicion[0]) and (a[1] == posicion[1]):
+                if a[2] == 1:
+                    print('Estado: '+str(estado)+' accion: arriba')
+                    return 'arriba'
+                elif a[2] == 2:
+                    print('Estado: '+str(estado)+' accion: abajo')
+                    return 'abajo'
+                elif a[2] == 3:
+                    print('Estado: '+str(estado)+' accion: izquierda')
+                    return 'izquierda'
+                elif a[2] == 4:
+                    print('Estado: '+str(estado)+' accion: derecha')
+                    return 'derecha'
+main()
---
+++ Tema 1/Ejercicio III/Rata.py
@@ -0,0 +1,83 @@
+#CONSTANTES
+VACIO = 0
+MURO = 1
+QUESO = 2
+RATA = 3
+
+IZQUIERDA = 0
+ARRIBA = 1
+DERECHA = 2
+ABAJO = 3
+INVALIDO = -1
+
+class Rata:
+  def __init__(self, fila, columna):
+    self.fila = fila
+    self.columna = columna
+    self.movimiento = -1 #valor inicial
+    self.tuvoExito = False #valor inicial
+
+  def verArriba(self, ambiente):
+    if self.fila <= 0:
+      return MURO
+    if ambiente[self.fila - 1][self.columna] == 1:
+      return MURO
+    return VACIO
+    
+  def verAbajo(self, ambiente):
+    if self.fila >= len(ambiente) - 1:
+      return MURO
+    if ambiente[self.fila + 1][self.columna] == 1:
+      return MURO
+    return VACIO
+
+  def verDerecha(self, ambiente):
+    if self.columna >= len(ambiente[0]) - 1:
+      return MURO
+    if ambiente[self.fila][self.columna + 1] == 1:
+      return MURO
+    return VACIO
+
+  def verIzquierda(self, ambiente):
+    if self.columna <= 0:
+      return MURO
+    if ambiente[self.fila][self.columna - 1] == 1:
+      return MURO
+    return VACIO
+
+  #Definir movimiento que debe realizar el agente según la tabla de asociación
+  def definirMovimiento(self, ambiente, decision):
+    self.movimiento = decision[self.verIzquierda(ambiente), 
+    self.verArriba(ambiente), self.verDerecha(ambiente), self.verAbajo(ambiente)]
+    self.tuvoExito = self.olerQueso(ambiente)
+    return self.movimiento
+  
+  #Cambiar la posición del agente en la matriz de ambiente y en lo que conoce de sí mismo
+  def moverse(self, ambiente):
+    ambiente[self.fila][self.columna] = VACIO
+    if self.movimiento == IZQUIERDA: #a la izquierda
+      self.columna = self.columna - 1
+    elif self.movimiento == ARRIBA: #a arriba
+      self.fila = self.fila - 1
+    elif self.movimiento == DERECHA: #a la derecha
+      self.columna = self.columna + 1
+    elif self.movimiento == ABAJO: #a abajo
+      self.fila = self.fila + 1
+    ambiente[self.fila][self.columna] = RATA
+
+  #Detectar si la casilla a la cual está a punto de moverse tiene el queso o no
+  def olerQueso(self, ambiente):
+    if self.movimiento == IZQUIERDA:
+      verFila = self.fila
+      verColumna = self.columna - 1
+    elif self.movimiento == ARRIBA:
+      verFila = self.fila - 1
+      verColumna = self.columna
+    elif self.movimiento == DERECHA:
+      verFila = self.fila
+      verColumna = self.columna + 1
+    elif self.movimiento == ABAJO:
+      verFila = self.fila + 1
+      verColumna = self.columna
+    #Revisar si hay queso en la casilla
+    return ambiente[verFila][verColumna] == QUESO
